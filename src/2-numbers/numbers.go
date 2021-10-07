4a8b36907fc77f7ed213fd3e27395b18ed15e2da
Param Siddharth <contact@paramsid.com>
2021-10-07 16:42:32 +0530

    संख्याएँ

---
+++ src/2-numbers/numbers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%c\n", x)
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%o\n", x)
+	fmt.Printf("%q\n", x)
+	fmt.Printf("%x\n", x)
+	fmt.Printf("%#x\n", x)
+	fmt.Printf("%X\n", x)
+	fmt.Printf("%#X\n", x)
+	fmt.Printf("%U\n", x)
+}
